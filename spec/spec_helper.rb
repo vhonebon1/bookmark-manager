8bef8e770ea3f6e4b1840b5b17236f849dce815f
Verity <vhonebon@gmail.com>
2017-06-30 14:27:49 +0100

    Made change to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 ENV['RACK_ENV'] = 'test'
-env = ENV['RACK_ENV']
 require 'capybara'
 require 'rspec'
 require 'capybara/rspec'
