a38253bac4f025dc5b2071cc60610468bed3ed47
Verity <vhonebon@gmail.com>
2017-06-30 10:36:48 +0100

    Procfile updated

--- app/data_mapper_config.rb
+++ app/data_mapper_config.rb
@@ -5,6 +5,7 @@ require 'dm-postgres-adapter'
 require_relative './models/link.rb'
 require_relative './models/tag.rb'
 
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
